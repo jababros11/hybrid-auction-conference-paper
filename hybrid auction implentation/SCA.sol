c81913fe9dd0c4d56b7ef602f44f609f0924d659
jababros11 <93003912+jababros11@users.noreply.github.com>
2024-03-04 17:22:25 +0530

    Add files via upload

--- hybrid auction implentation/SCA.sol
+++ hybrid auction implentation/SCA.sol
@@ -5,11 +5,11 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Submission {
 
-    uint256[] pb ;
+    string pb ;
 
     
     // this part performs the part of first half where auctioneer gets the encrypted value of the bidders and adds half of its random share
-    function bidderEncryptedBidSubmission(uint256[] memory slices ) public {
+    function bidderEncryptedBidSubmission(string memory slices ) public {
         pb = slices;
     }
     // function AuctioneerRetrivesBidder1EncryptedData() public view returns (uint256){
@@ -20,7 +20,7 @@ contract Submission {
     // }
 
 
-    function retrieveBidderEncryptedBidSubmission() public view returns (uint256[] memory){
+    function retrieveBidderEncryptedBidSubmission() public view returns (string memory){
         return pb;
     }
 
