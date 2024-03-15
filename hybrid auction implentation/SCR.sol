cc750f4c5235c4110ca22405c22b0de09fd19a7e
jababros11 <93003912+jababros11@users.noreply.github.com>
2024-03-15 05:19:42 -0700

    Add files via upload

--- hybrid auction implentation/SCR.sol
+++ hybrid auction implentation/SCR.sol
@@ -1,20 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.8.2 <0.9.0;
-
-/**
- * @title Storage
- * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
- */
 contract Registration {
-
     string storeEncryptedUID;
-
-    // /**
-    //  * @dev Store value in variable
-    //  * @param num value to store
-    //  */
     function store(string memory UID) public {
         storeEncryptedUID = UID;
     }
