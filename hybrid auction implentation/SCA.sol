709e3ca3294757ec3ff2b6eed98fc938e71c2cda
jababros11 <93003912+jababros11@users.noreply.github.com>
2024-03-18 05:27:33 -0700

    Add files via upload

--- hybrid auction implentation/SCA.sol
+++ hybrid auction implentation/SCA.sol
@@ -6,41 +6,24 @@ pragma solidity >=0.8.2 <0.9.0;
 contract Submission {
 
     string pb ;
+    string VariableForBidder;
 
     
-    // this part performs the part of first half where auctioneer gets the encrypted value of the bidders and adds half of its random share
+    // this part performs the part of first half where auctioneer gets the encrypted value
+    //  of the bidders and adds half of its random share
     function bidderEncryptedBidSubmission(string memory slices ) public {
         pb = slices;
     }
-    // function AuctioneerRetrivesBidder1EncryptedData() public view returns (uint256){
-    //     return pb12;
-    // }
-    // function AuctioneerRetrievesBidder2EncryptedData() public view returns (uint256){
-    //     return pb21;
-    // }
-
 
     function retrieveBidderEncryptedBidSubmission() public view returns (string memory){
         return pb;
     }
 
-
-
-
-
-
-
-    // function Inbidder1DataAuctioneerAddsHalfRandomNumber(uint256 num) public {
-    //     pb12 = num;
-    // }
-    // function Inbidder2DataAuctioneerAddsHalfRandomNumber(uint256 num) public {
-    //     pb21 = num;
-    // }
-    // function bidder1AddsRandomNumberInBidder2Data(uint256 num) public {
-    //     pb12 = num;
-    // }
-    // function bidder2AddsRandomNumberInBidder1data(uint256 num) public {
-    //     pb21 = num;
-    // }
-
+    function updateBidderVariable(string memory auctioneerPB) public{
+        VariableForBidder =  auctioneerPB;
+    }
+    function fetchVariable() public view returns(string memory){
+        return VariableForBidder;
+    }
 }
+
